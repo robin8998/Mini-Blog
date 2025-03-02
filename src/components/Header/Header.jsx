d263c1edf1f9438c91f374c6929636f9063db257
Robin Singh <ranarobin8998@gmail.com>
2025-03-02 21:04:41 +0530

    beautify header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,6 +8,7 @@ import { Menu, X } from 'lucide-react';
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [currentItem,setCurrentItem] = useState("Home")
   const authStatus = useSelector((state) => state.auth.status);
   const navigate = useNavigate();
 
@@ -15,27 +16,32 @@ function Header() {
     {
       name: 'Home',
       slug: "/",
-      active: true
+      active: true,
+      IsCurrent : currentItem
     },
     {
       name: "Login",
       slug: "/login",
       active: !authStatus,
+      IsCurrent : currentItem
     },
     {
       name: "Signup",
       slug: "/signup",
       active: !authStatus,
+      IsCurrent : currentItem
     },
     {
       name: "All Posts",
       slug: "/all-posts",
       active: authStatus,
+      IsCurrent : currentItem
     },
     {
       name: "Add Post",
       slug: "/add-post",
       active: authStatus,
+      IsCurrent : currentItem
     },
   ];
 
@@ -43,6 +49,10 @@ function Header() {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const  currentSection = (item) => {
+     navigate(item.slug)
+     setCurrentItem(item.name)
+  }
   return (
     <header className='sticky top-0 z-50 bg-gradient-to-r from-gray-700 to-gray-600 shadow-lg'>
       <Container>
@@ -61,8 +71,8 @@ function Header() {
                 item.active ? (
                   <li key={item.name}>
                     <button
-                      onClick={() => navigate(item.slug)}
-                      className='inline-block px-4 py-2 text-white font-medium rounded-md transition-all duration-300 hover:bg-gray-800 hover:shadow-md active:scale-95'
+                      onClick={() => (currentSection(item))}
+                      className={`inline-block px-4 py-2 ${item.IsCurrent === item.name ? "text-orange-500 font-semibold" : "text-white font-medium"} rounded-md transition-all duration-300 hover:bg-gray-800 hover:shadow-md active:scale-95`}
                     >
                       {item.name}
                     </button>
